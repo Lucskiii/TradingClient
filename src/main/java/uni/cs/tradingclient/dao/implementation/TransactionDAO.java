fa9a5a2532603a98949b7f9ca250ae1f50be103b
Lucskiii <160024003+Lucskiii@users.noreply.github.com>
2025-04-09 15:37:02 +0200

    create TransactionDAO

---
+++ src/main/java/uni/cs/tradingclient/dao/implementation/TransactionDAO.java
@@ -0,0 +1,68 @@
+package uni.cs.tradingclient.dao.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import uni.cs.tradingclient.model.Transaction;
+import uni.cs.tradingclient.persistence.CommunicationHandler;
+
+/**
+ *
+ * @author lucakoelzsch
+ */
+public class TransactionDAO {
+
+    private CommunicationHandler handler;
+
+    public TransactionDAO() {
+        handler = new CommunicationHandler();
+    }
+
+    public List<Transaction> getAllTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+        List<Map<String, Object>> data = handler.executeQuery("SELECT * FROM Transaction");
+
+        for (Map<String, Object> map : data) {
+            Transaction transaction = new Transaction(
+                    (Integer) map.get("Transaction_ID"),
+                    (Integer) map.get("Portfolio_ID"),
+                    (String) map.get("ISIN"),
+                    (Double) map.get("Transaction_Price"),
+                    (Integer) map.get("Quantity_Change")
+            );
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
+
+    public Transaction getTransactionById(int transactionID) {
+        List<Map<String, Object>> data = handler.executeQuery("SELECT * FROM Transaction WHERE Transaction_ID = ?", transactionID);
+        if (data.isEmpty()) {
+            return null;
+        }
+        Map<String, Object> map = data.get(0);
+        return new Transaction(
+                (Integer) map.get("Transaction_ID"),
+                (Integer) map.get("Portfolio_ID"),
+                (String) map.get("ISIN"),
+                (Double) map.get("Transaction_Price"),
+                (Integer) map.get("Quantity_Change")
+        );
+    }
+
+    public boolean saveTransaction(Transaction transaction) {
+        String sql = "INSERT INTO Transaction (Portfolio_ID, ISIN, Transaction_Price, Quantity_Change) VALUES (?, ?, ?, ?)";
+        return handler.executeUpdate(sql, transaction.getPortfolioID(), transaction.getISIN(), transaction.getTransactionPrice(), transaction.getQuanityChnge());
+    }
+
+    public boolean updateTransaction(Transaction transaction) {
+        String sql = "UPDATE Transaction SET Portfolio_ID = ?, ISIN = ?, Transaction_Price = ?, Quantity_Change = ? WHERE Transaction_ID = ?";
+        return handler.executeUpdate(sql, transaction.getPortfolioID(), transaction.getISIN(), transaction.getTransactionPrice(), transaction.getQuanityChnge(), transaction.getTransactionID());
+    }
+
+    public boolean deleteTransaction(int transactionID) {
+        String sql = "DELETE FROM Transaction WHERE Transaction_ID = ?";
+        return handler.executeUpdate(sql, transactionID);
+    }
+}
+
